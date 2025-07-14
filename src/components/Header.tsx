e98497723959528fcc66c94030a0704665fe0268
Sreeparna <sreeparnadas0675@gmail.com>
2025-07-15 00:10:46 +0530

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import backgroundImage from '../assets/images/background.jpg';
+import profilePhoto from '../assets/images/profile-photo.jpg';
+
+function Header(){
+    return (
+        <>
+            <div className="container">
+                <div className=" d-flex flex-column align-items-center" 
+                style={{
+                        height: '35vh',
+                        background: 'linear-gradient(90deg, rgb(167 165 207) 0%, rgba(9, 9, 121, 1) 24%, rgba(0, 212, 255, 1) 100%)'
+                    }}
+                >
+                    <div>
+                        <img src={profilePhoto} alt="..." className="profile-photo rounded-circle img-fluid"/>
+                    </div>
+                     <div className='text-white fs-4'>
+                        Sreeparna Das
+                    </div>
+                    <div className='text-white fs-6'>
+                        Full Stack Developer
+                    </div>
+
+                    <div className='text-white pt-5'>
+                        I build exceptional and accessible digital experiences for the web. Focused on creating elegant solutions to complex problems.
+                    </div>
+                </div>
+
+                    
+            </div>
+        
+        </>
+    )
+}
+
+export default Header;
---
+++ src/data.tsx
@@ -0,0 +1,10 @@
+//function component use PascalCasing
+
+
+function Data(){
+    //Jsx: Javascript XML
+
+    return <h1>This is my first rect project</h1>;
+}
+
+export default Data;
