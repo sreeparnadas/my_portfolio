5d46609e365c67365763a1cf637baa42f24d6373
Sreeparna <sreeparnadas0675@gmail.com>
2025-07-29 00:16:11 +0530

    implemented emailjs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,59 @@
-function Contact(){
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_43tc7h9', 'template_43mf7zt', form.current, {
+        publicKey: 'ZCiR3-5STB_hcTSvn',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    };
     return (
         <>
-           Contact
+           <h3 className='text-center pt-5'>Let's work together 📩</h3>
+
+           <div className='d-flex justify-content-center mb-5'>
+                <div className="card w-50">
+                    <div className="card-header text-info fs-3 text-center">
+                        Contact me
+                    </div>
+                    <div className="card-body">
+                        <form className='d-flex flex-column' ref={form}>
+                            <div className='form-group'>
+                                <label>Full Name:</label>
+                                <input type='text' className='form-control' name='user_name'></input>
+                            </div>
+                            <div className='form-group'>
+                                <label >E-mail:</label>
+                                <input type='text' className='form-control' name='user_email'></input>
+                            </div>
+                            <div className='form-group'>
+                                <label>Subject:</label>
+                                <input type='text' className='form-control' name='subject'></input>
+                            </div>
+                            <div className='form-group mt-3'>
+                                <textarea aria-label="With textarea" className='form-control' name='message'></textarea>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <button type="button" className="btn btn-success mt-3" name='send_mail' id='send-mail-btn' onClick={sendEmail}>Send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>  
+           </div>
+            
         </>
     )
 }
